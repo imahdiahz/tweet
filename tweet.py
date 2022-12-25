3a440ba8373017fad2d184617f273da4f7efa9d1
root <mahdi@mahdi.com>
2022-12-25 16:15:43 +0330

    Fix Bugs

--- tweet.py
+++ tweet.py
@@ -3,16 +3,16 @@ from pyrogram.types import Message
 from pyrogram.types import (ReplyKeyboardMarkup, InlineKeyboardMarkup, InlineKeyboardButton)
 from config import api_id,api_hash,bot_token,channel
 
-bot = Client("OdinGame_Tweet",api_id,api_hash,bot_token)
+bot = Client("OdinGame_Tweet",api_id,api_hash,bot_token=bot_token)
 
 async def odingame():
     dsh = InlineKeyboardMarkup([
             [InlineKeyboardButton(f'|📝| - بیانیه', callback_data='tw')]])
-    return await dsh
+    return dsh
 
 @bot.on_message(filters.command(['/panel',"/start"],None))
 async def panel(c,m):
-	await bot.send_message(m.chat.id,f"**• بخش مورد نظرتون رو انتخاب کنین **",reply_markup=odingame(),reply_to_message_id=m.message_id)
+	await bot.send_message(m.chat.id,f"**• بخش مورد نظرتون رو انتخاب کنین **",reply_markup=await odingame(),reply_to_message_id=m.message_id)
 
 @bot.on_callback_query(filters.regex(r"tw"))
 async def p_q_c(c,query):
@@ -22,7 +22,7 @@ async def p_q_c(c,query):
 @bot.on_message(filters.group,group=0)
 async def sendch(c, m):
     if m.reply_to_message and "بیانیه خود را ارسال کنید" in m.reply_to_message.text:
-        mb_id = (m.forward(channel)).message_id
+        mb_id = await m.forward(channel)
         await bot.send_message(m.chat.id,"**• انجام شد✔**")
 
 print("OdinGame Tweet Bot Starting...")
