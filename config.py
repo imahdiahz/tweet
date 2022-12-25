cf58118960313884184e2e80f2167560d8fbff02
root <mahdi@mahdi.com>
2022-12-25 16:13:18 +0330

    fix config

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 api_id = 0 # Get from my.telegram.org
 api_hash = "xyz" # Get from my.telegram.org
-channel = "-10010000000" # Get from your channel
+channel = "@" # Ex: @Odin_Game
 bot_token = "0000:xyz" # Get from BotFather
